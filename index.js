6c310f59d973cff0ca983c439634347fea13281d
gabangsr <gabangsram@gmail.com>
2024-12-03 21:56:37 +0800

    Added a front page, for further changing

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const port = 3000;
-const db = require('./database');
+const db = require('./database'); 
+const path = require('path');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log('Root route accessed'); 
-    res.send('Hello World');
+    console.log('Root route accessed');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/show', (req, res) => {
@@ -18,12 +19,12 @@ app.get('/show', (req, res) => {
 app.get('/database', (req, res) => {
     db.all('SELECT * FROM items', [], (err, rows) => {
         if (err) {
+            console.error('Database error:', err);
             return res.status(500).send('Error fetching items from the database');
         }
         res.json(rows);
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
